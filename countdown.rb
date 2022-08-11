d45c3876fef96997b098c92ea06aedc91b938c71
Garret Gallant <gallanttechnological@gmail.com>
2022-08-11 15:57:27 -0600

    lesson complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  while num > 0 do
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    if num == 0 
+      return "HAPPY NEW YEAR!"
+    end
+  end
 end
+
+def countdown_with_sleep(num)
+  sleep 5
+  puts num
+end
+
